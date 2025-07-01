75cc0cf67c84ec1af85994e5d4f0b582c3f7c066
chrnmndti <charancuz008@gmail.com>
2025-07-01 19:49:31 +0530

    oof

---
+++ tmplt/template.cpp
@@ -0,0 +1,16 @@
+//charan_0
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll; 
+const char nl='\n';
+const char spc=' ';
+int main(){
+
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+int t;
+cin>>t;
+while(t--){}
+return 0;}
