043208206b3568687efc1ed24172ac26f653aade
chrnz008 <charancuz008@gmail.com>
2026-07-12 21:21:27 +0530

    add emacs config mehh

---
+++ .emacs.el
@@ -0,0 +1,21 @@
+;; vim is bettern
+
+(tool-bar-mode 0)
+(load-theme 'deeper-blue t)
+
+;; faces
+(set-face-background 'fringe "background at point")
+;; (set-face-background 'mode-line "background at point")
+;; (set-face-foreground 'mode-line "foreground at point")
+
+;; key bindings
+;; (keymap-global-set "C-=" 'text-scale-increase) ;; C-x c-= does the samee
+
+;; notes
+;; use c-x c-m-{0,+,=,-} for global text scale
+;; take a look at icomplete-mode
+
+;; Display/Text Display
+(setq-default tab-width 4)
+(setq auto-save-default nil)
+(setq make-backup-files nil)
