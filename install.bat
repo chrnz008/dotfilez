b0ce5bc5e888dadaa4d8027ab62271314dba9ada
chrnz008 <charancuz008@gmail.com>
2026-08-24 11:21:47 +0530

    fix absolute path handling:

--- install.bat
+++ install.bat
@@ -3,13 +3,13 @@
 setlocal
 
 REM alacritty
-mklink /D "%APPDATA%\alacritty" "%~dp0%\.config\alacritty"
+mklink /D "%APPDATA%\alacritty" "%~dp0.config\alacritty"
 
 REM yazi
-mklink /D "%APPDATA%\yazi\config" "%~dp0%\.config\yazi"
+mklink /D "%APPDATA%\yazi\config" "%~dp0.config\yazi"
 
 REM vim
-mklink /D "%USERPROFILE%\vimfiles" "%~dp0%\.config\vim"
+mklink /D "%USERPROFILE%\vimfiles" "%~dp0.config\vim"
 
 REM TODO
 REM caps.reg
