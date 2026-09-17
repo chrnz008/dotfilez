51fcc59ef6751f3132b14e5406e25eddee7e98f5
chrnz008 <charancuz008@gmail.com>
2026-09-17 21:25:02 +0530

    vim:add yazi integration,install: add wtabs

--- install.bat
+++ install.bat
@@ -5,11 +5,17 @@ setlocal
 REM alacritty
 mklink /D "%APPDATA%\alacritty" "%~dp0.config\alacritty"
 
+REM vim
+mklink /D "%USERPROFILE%\vimfiles" "%~dp0.config\vim"
+
 REM yazi
 mklink /D "%APPDATA%\yazi\config" "%~dp0.config\yazi"
 
-REM vim
-mklink /D "%USERPROFILE%\vimfiles" "%~dp0.config\vim"
+REM wtabs
+set wtabsurl=https://github.com/leafOfTree/WindowTabs/releases/download/v2025.06.30/WindowTabs.exe
+set shell.startup=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup
+curl -fLO "%wtabsurl%" --output-dir "%shell.startup%"
+
 
 REM TODO
 REM caps.reg
